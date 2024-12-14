b772a7d9c41d07c188811b00bdbfa278ddefc56c
venkideshVenu <venkidesh.venu.04@gmail.com>
2024-12-15 00:02:01 +0530

    old news page added

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import tech_news_view, tech_news_detail_view
+from .views import tech_news_view, tech_news_detail_view, old_tech_news_view
 
 urlpatterns = [
     path('', tech_news_view, name='tech_news'),
+    path('old/', old_tech_news_view, name='old_tech_news'),
     path('<int:pk>/', tech_news_detail_view, name='tech_news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import TechArticle
 from .services import NewsAPIService
+from django.core.cache import cache
 
 def tech_news_view(request):
-    NewsAPIService.fetch_tech_news(max_articles=6)
-    articles = TechArticle.objects.order_by('-published_at')[:10]
+    cached_articles = cache.get('tech_news_articles')
+    
+    if not cached_articles:
+        # Fetch new articles if not in cache
+        NewsAPIService.fetch_tech_news(max_articles=9)
+
+        articles = TechArticle.objects.order_by('-published_at')[:12]
+        # Cache the articles for 1 hour
+        cache.set('tech_news_articles', articles, 3600)
+    else:
+        articles = cached_articles
+      
     return render(request, 'news/tech_news.html', {'articles': articles})
+
+
+def old_tech_news_view(request):
+    articles = TechArticle.objects.order_by('-published_at')[:]
+    return render(request, 'news/old_news.html', {'articles': articles})
 
 
 def tech_news_detail_view(request, pk):
