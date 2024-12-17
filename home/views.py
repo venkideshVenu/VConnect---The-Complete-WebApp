19d5b7984bfdd762aae1c6282e5ff6baf1053c10
venkideshVenu <venkidesh.venu.04@gmail.com>
2024-12-17 23:51:49 +0530

    home templates modified

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def get_home_page(request):
-    return render(request, 'home/base.html')
+    return render(request, 'home/index.html')
